fdc48153bf278e65f6192555ff48f88e93675c15
sujoyghosal2003 <sujoyghosal445@gmail.com>
2024-09-20 10:40:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+import PackingList from './components/PackingList'
+function App() {
+
+  return (
+    <>
+      
+      <PackingList message="Now I'm learning ReactJS!" />
+    </>
+  )
+}
+
+export default App
+
